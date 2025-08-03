f9624b5168e2bd07c0b9e8f2da4952e6bb5dff1b
andrii_solomka <andriissolomka@gmail.com>
2025-08-03 14:57:53 +0300

    feat: add arch tests

--- libs/src/common/index.ts
+++ libs/src/common/index.ts
@@ -1,7 +1,5 @@
 export * from './config/logger.config';
 export * from './config/redis.config';
 export * from './errors/http.error';
-// export * from './msw/setup';
-// export * from './msw/test.server';
 export * from './utils/logger/pino.setup';
 export * from './proto/generated/weather/v1/weather_pb';
