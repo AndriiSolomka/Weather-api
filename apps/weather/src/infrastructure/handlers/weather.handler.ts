116412583db8c0b4b2af7648614324dcacd8c9d3
andrii_solomka <andriissolomka@gmail.com>
2025-08-06 17:39:50 +0300

    refactor: simplify WeatherRoutes

--- apps/weather/src/infrastructure/handlers/weather.handler.ts
+++ apps/weather/src/infrastructure/handlers/weather.handler.ts
@@ -1,20 +1,9 @@
-import { create } from '@bufbuild/protobuf';
-import {
-  GetWeatherRequest,
-  GetWeatherResponse,
-  GetWeatherResponseSchema,
-} from '@weather-app/libs';
+import { GetWeatherRequest } from '@weather-app/libs';
 
 import { WeatherInterface } from '../../core/weather.interface';
 
 export function getWeatherHandler(weatherService: WeatherInterface) {
-  return async (req: GetWeatherRequest): Promise<GetWeatherResponse> => {
-    const weatherData = await weatherService.getWeather(req.city);
-
-    return create(GetWeatherResponseSchema, {
-      temperature: weatherData.temperature,
-      humidity: weatherData.humidity,
-      description: weatherData.description,
-    });
+  return async (req: GetWeatherRequest) => {
+    return await weatherService.getWeather(req.city);
   };
 }
--- apps/weather/src/interface/weather.route.ts
+++ apps/weather/src/interface/weather.route.ts
@@ -3,14 +3,13 @@ import { WeatherService } from '@weather-app/libs';
 
 import { ConnectRouteInterface } from './routes.interface';
 import { WeatherInterface } from '../core/weather.interface';
-import { getWeatherHandler } from '../infrastructure/handlers/weather.handler';
 
 export class WeatherRoutes implements ConnectRouteInterface {
   constructor(private readonly weatherService: WeatherInterface) {}
 
   register(router: ConnectRouter): void {
     router.service(WeatherService, {
-      getWeather: getWeatherHandler(this.weatherService),
+      getWeather: ({ city }) => this.weatherService.getWeather(city),
     });
   }
 }
